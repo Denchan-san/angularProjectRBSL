23ba07371ebe805388d52d28f9fad162547a246b
Jokir <doner24072004@gmail.com>
2024-06-09 20:25:42 +0300

    Adding Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,30 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
 recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe','This is a simply recipe', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272'),
-        new Recipe('A Test Recipe 2','This is a simply recipe 2', 'https://images.immediate.co.uk/production/volatile/sites/30/2024/04/MonteCristoSandwich-7cbdfe9.jpg?quality=90&webp=true&resize=300,272')
+        new Recipe(
+            'Ragu',
+            'This is a simply recipe',
+            'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('Vegs', 20),
+            ]
+        ),
+        new Recipe(
+            'Sandwiches',
+            'This is a simply recipe 2',
+            'https://images.immediate.co.uk/production/volatile/sites/30/2024/04/MonteCristoSandwich-7cbdfe9.jpg?quality=90&webp=true&resize=300,272',
+            [
+                new Ingredient('Ham', 1),
+                new Ingredient('Bread', 2),
+                new Ingredient('Cheese',1)
+            ]
+        )
       ];
 
     getRecipes() {
