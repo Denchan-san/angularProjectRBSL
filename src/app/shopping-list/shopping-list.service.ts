7ecc8c37f04c4182c1c8074f468f7930c616621d
Jokir <doner24072004@gmail.com>
2024-06-09 19:52:07 +0300

    Adding the Shopping List Service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,21 @@
+import { Ingredient } from "../shared/ingredient.model";
+
 export class ShoppingListService {
-    
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10),
+        new Ingredient('Cucumbers', 15),
+      ];
+
+    addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+    }
+
+    onIngredientDelete() {
+
+    }
+
+    getIngredients() {
+        return this.ingredients.slice();
+    }
 }
