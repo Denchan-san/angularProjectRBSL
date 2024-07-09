f9ae03f9a685d989c1690fff5d8aba6c860a45f2
Jokir <doner24072004@gmail.com>
2024-07-09 14:36:01 +0300

    Resetting the Form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -39,7 +39,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy{
     this.subscription.unsubscribe();
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     
@@ -49,5 +49,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy{
     else{
       this.shoppingListService.addIngredient(newIngredient);
     }
+    this.editMode = false;
+    form.reset();
   }
 }
