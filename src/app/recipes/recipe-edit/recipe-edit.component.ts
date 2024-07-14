a259c6a0faf144dc9067f18f06291372ca5dde6d
Jokir <doner24072004@gmail.com>
2024-07-14 17:56:37 +0300

    Redirecting the User (After Deleting a Recipe)

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Form, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { RecipeService } from '../recipe.service';
-import { Ingredient } from '../../shared/ingredient.model';
-import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -53,24 +51,24 @@ export class RecipeEditComponent implements OnInit{
     let recipeImagePath = '';
     let recipeDescription = '';
     let recipeIngredients = new FormArray([]);
-    
-    if(this.editMode){
+
+    if (this.editMode) {
       const recipe = this.recipeService.getRecipe(this.id);
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
       recipeDescription = recipe.description;
-      if(recipe['ingredients']) {
-        recipe.ingredients.forEach(ingredient => {
+      if (recipe['ingredients']) {
+        for (let ingredient of recipe.ingredients) {
           recipeIngredients.push(
             new FormGroup({
               'name': new FormControl(ingredient.name, Validators.required),
               'amount': new FormControl(ingredient.amount, [
                 Validators.required,
-                Validators.pattern(/^1[1-9]+[0-9]*$/)
-              ] )
+                Validators.pattern(/^[1-9]+[0-9]*$/)
+              ])
             })
-          )
-        });
+          );
+        }
       }
     }
 
@@ -92,7 +90,7 @@ export class RecipeEditComponent implements OnInit{
         'name': new FormControl(null, Validators.required),
         'amount': new FormControl(null, [
           Validators.required,
-          Validators.pattern(/^1[1-9]+[0-9]*$/)
+          Validators.pattern(/^[1-9]+[0-9]*$/)
         ])
       })
     );
