140ebf95d0dff7d9967f7cb64d7e946e6c020d91
Jokir <doner24072004@gmail.com>
2024-07-29 17:22:43 +0300

    Storing Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,23 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
+import { RecipeService } from "../recipes/recipe.service";
+
 //need to add if u want to inject service into other serice (httpService -> this service)
 @Injectable({providedIn:'root'}) 
 export class DataStorageService {
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient,
+                private recipeService: RecipeService) {}
+
+    storeRecipes() {
+        const recipes = this.recipeService.getRecipes();
+        this.http
+         .put(
+           'https://ng-course-recipe-book-df794-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+            recipes
+         )
+         .subscribe( response => {
+            console.log(response);
+         });
+    }
 }
