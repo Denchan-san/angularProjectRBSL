38a3662ed3e9f66cbe3bde981d20c5288503bab2
Jokir <doner24072004@gmail.com>
2024-07-30 19:39:05 +0300

    Fixing a Bug with the Resolver

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -3,13 +3,22 @@ import { ActivatedRouteSnapshot, MaybeAsync, Resolve, RouterStateSnapshot } from
 
 import { Recipe } from "./recipe.model";
 import { DataStorageService } from "../shared/data-storage.service";
+import { RecipeService } from "./recipe.service";
 
 @Injectable({providedIn: 'root'})
 export class RecipesResolverService implements Resolve<Recipe[]> { 
 
-    constructor(private dataStorageService : DataStorageService) {}
+    constructor(private dataStorageService : DataStorageService,
+                private recipesService: RecipeService
+    ) {}
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<Recipe[]> {
-        return this.dataStorageService.fetchRecipes();
+        const recipes = this.recipesService.getRecipes();
+
+        if(recipes.length === 0) {
+            return this.dataStorageService.fetchRecipes();
+        } else {
+            return recipes;
+        }
     }
 }
