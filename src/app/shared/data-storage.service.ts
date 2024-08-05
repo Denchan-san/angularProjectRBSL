b9c8dd87904d1c91518369f2acf6698080fd4b93
Jokir <doner24072004@gmail.com>
2024-08-05 18:55:35 +0300

    Adding the Token to Outgoing Requests

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,50 +1,57 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
-import { map, tap } from "rxjs";
+import { exhaustMap, map, take, tap } from 'rxjs';
 
-import { RecipeService } from "../recipes/recipe.service";
-import { Recipe } from "../recipes/recipe.model";
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 //need to add if u want to inject service into other serice (httpService -> this service)
-@Injectable({providedIn:'root'}) 
+@Injectable({ providedIn: 'root' })
 export class DataStorageService {
-    constructor(private http: HttpClient,
-                private recipeService: RecipeService) {}
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
-    storeRecipes() {
-        const recipes = this.recipeService.getRecipes();
-        this.http
-         .put(
-           'https://ng-course-recipe-book-df794-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
-            recipes
-         )
-         .subscribe( response => {
-            console.log(response);
-         });
-    }
-    
-    fetchRecipes() {
-        return this.http
-          .get<Recipe[]>(
-            'https://ng-course-recipe-book-df794-default-rtdb.europe-west1.firebasedatabase.app/recipes.json'
-          )
-          .pipe(map(recipes => {
-              return recipes.map(recipe => {
-                return {
-                  ...recipe,
-                   ingredients: recipe.ingredients ? recipe.ingredients : []
-                  };
-              });
-            }),
-            tap(recipes => {
-              console.log('getting recipes..');
-              this.recipeService.setRecipes(recipes);
-            })
-          );
-          // .subscribe(recipes => {
-          //   console.log('getting recipes..');
-          //   this.recipeService.setRecipes(recipes);
-          // });
-      }
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        'https://ng-course-recipe-book-df794-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+        recipes
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+
+  fetchRecipes() {
+    //take(1) will unsubscribe
+    return this.authService.user.pipe(
+      take(1),
+      exhaustMap((user) => {
+        return this.http.get<Recipe[]>(
+          'https://ng-course-recipe-book-df794-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+          {
+            params: new HttpParams().set('auth', user.token),
+          }
+        );
+      }),
+      map((recipes) => {
+        return recipes.map((recipe) => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : [],
+          };
+        });
+      }),
+      tap((recipes) => {
+        console.log('getting recipes..');
+        this.recipeService.setRecipes(recipes);
+      })
+    );
+  }
 }
